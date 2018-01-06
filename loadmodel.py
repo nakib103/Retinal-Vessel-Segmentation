33f1dcc8b72540fc90e578185876b241a7cdaa4f
Syed Nakib Hossain <syedhossain103@gmail.com>
2018-01-06 18:55:34 +0600

    Delete loadmodel.py

--- loadmodel.py
+++
@@ -1,7 +0,0 @@
-from keras.models import Sequential, model_from_json
-
-model = model_from_json(open('DRIVE_Logistic_model.json').read())# if json 
-# model = model_from_yaml(open('my_model_architecture.yaml').read())# if yaml 
-model.load_weights('DRIVE_Logistic_wts.h5')
-
-print(model.metrics_names)
